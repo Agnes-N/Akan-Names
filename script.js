a1bb7788240eb3e616840aa18299e5b8e06a13d5
Agnes Nyiramisigaro <aggyreina@gmail.com>
2019-07-19 18:12:08 +0200

    array of males'names and female's names are created

--- script.js
+++ script.js
@@ -8,4 +8,7 @@ function weekdays(){
     var CC = (yy - 1) / 100 + 1;
     var birthDay = (CC / 4 - 2 * CC - 1 + 5 * yy / 4 + 26 * (mm + 1) / 10 + dd) % 7;
     document.getElementById("display").innerHTML = Math.round(birthDay);
+
+    var malesNames = ["kwasi", "kwadwo", "kwabena", "kwaku", "yaw", "kofi", "kwame"];
+    var femalesNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 }
