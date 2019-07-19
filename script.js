fca430b370cc108c0d7db0647fab79c0baa8182c
Agnes Nyiramisigaro <aggyreina@gmail.com>
2019-07-19 18:09:59 +0200

    century and the day of the week whose a person is born on are calculated

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function weekdays(){
-    
+    var day = document.getElementById("day").value;
+    var enterDate = day.split("-");
+    var dd = parseInt(enterDate[2]);
+    var mm = parseInt(enterDate[1]);
+    var yy = parseInt(enterDate[0]);
+
+    var CC = (yy - 1) / 100 + 1;
+    var birthDay = (CC / 4 - 2 * CC - 1 + 5 * yy / 4 + 26 * (mm + 1) / 10 + dd) % 7;
+    document.getElementById("display").innerHTML = Math.round(birthDay);
 }
